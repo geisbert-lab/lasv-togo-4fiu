1a5f7e06806cad56f566487ae17f2c56209164ab
Jackie Turcinovic <jacquelynturcinovic@gmail.com>
2025-04-10 10:02:25 -0500

    Updates to include IPA and DCQ

--- limma.r
+++ limma.r
@@ -263,6 +263,14 @@ write.csv(dexp, "analysis/results-controls.csv",
           row.names=FALSE, na="")
 rm(m, c)
 
+# write IPA input
+dexp %>%
+  select(gene, lfc, padj, Comparison) %>%
+  reshape2::melt(id.vars=c("gene", "Comparison")) %>%
+  mutate(Header=paste0(Comparison, ".", variable)) %>%
+  reshape2::dcast(gene ~ Header, value.var="value") %>%
+  write.csv("analysis/ipa-input-controls.csv", row.names=FALSE)
+
 # total DE genes
 tot.degs <- dexp %>%
             filter(Significant) %>%
@@ -321,6 +329,14 @@ dexp <- list(diffexpr(c, m, makeContrasts(Middle - Baseline, levels=colnames(m))
 write.csv(dexp, "analysis/results-treated.csv",
           row.names=FALSE, na="")
 rm(x, m, c)
+
+# write IPA input
+dexp %>%
+  select(gene, lfc, padj, Comparison) %>%
+  reshape2::melt(id.vars=c("gene", "Comparison")) %>%
+  mutate(Header=paste0(Comparison, ".", variable)) %>%
+  reshape2::dcast(gene ~ Header, value.var="value") %>%
+  write.csv("analysis/ipa-input-treated.csv", row.names=FALSE)
 
 # total DE genes (main figure B)
 main.fig$B <- dexp %>%
@@ -383,7 +399,7 @@ ggsave("analysis/volcano-treated.png",
        units="in", width=7.5, height=3)
 
 # clean up
-rm(dexp, topgenes)
+rm(dexp, topgenes, tot.degs)
 
 ## compare treated vs. controls ------------------------------------------------
 # baseline
@@ -440,6 +456,14 @@ dexp <- dexp %>%
 write.csv(dexp, "analysis/results-comparison.csv",
           row.names=FALSE, na="")
 rm(x, m, c)
+
+# write IPA input
+dexp %>%
+  select(gene, lfc, padj, Comparison) %>%
+  reshape2::melt(id.vars=c("gene", "Comparison")) %>%
+  mutate(Header=paste0(Comparison, ".", variable)) %>%
+  reshape2::dcast(gene ~ Header, value.var="value") %>% 
+  write.csv("analysis/ipa-input-comparison.csv", row.names=FALSE)
 
 # total DE genes (main figure C)
 main.fig$C <- dexp %>%
@@ -528,35 +552,230 @@ ggsave("analysis/volcano-comparison-transitionallate.png",
        units="in", width=3.75, height=3)
 rm(dexp, topgenes)
 
-## assemble figures ------------------------------------------------------------
-# main figure is a 2x2 grid
-cowplot::plot_grid(plotlist=main.fig, labels="AUTO")
-ggsave("analysis/figure-main.png", units="in", width=7.5, height=6)
+## cell type scoring -----------------------------------------------------------
+# load annotations
+anno <- read.csv("annotations.csv", na.strings="") %>%
+        filter(!is.na(Cell.Type)) %>%
+        select(Gene, Cell.Type)
 
+# get log2CPM 
+logcpm <- cmat %>%
+          normalize.counts() %>%
+          cpm() %>%
+          log2()
 
+# subset genes -- some may be filtered out in the normalization process
+genelist <- intersect(rownames(logcpm), anno$Gene)
+anno <- filter(anno, Gene %in% genelist)
+logcpm <- logcpm[genelist, ]
+rm(genelist)
 
-# plot samples (supplemental A)
-supl.fig$A <- meta %>%
-  ggplot(aes(DPI, NHP, fill=Group, shape=Treatment)) +
-  geom_line(aes(group=NHP)) +
-  geom_point(size=3) +
-  scale_shape_manual(values=shapes.treat, ) +
+# get mean log2CPM per cell type
+anno <- logcpm %>%
+        as.data.frame() %>%
+        rownames_to_column("Gene") %>%
+        # melt to long
+        reshape2::melt(id.vars="Gene",
+                       variable.name="ID",
+                       value.name="log2CPM") %>%
+        # add in cell types
+        left_join(anno, by="Gene") %>%
+        # compile to mean logCPM by cell type
+        group_by(ID, Cell.Type) %>%
+        summarise(log2CPM=mean(log2CPM),
+                  .groups="drop") %>%
+        # add in metadata
+        left_join(meta, by="ID")
+
+# plot cell types: controls only
+anno %>%
+  filter(Treatment=="Control") %>%
+  ggplot(aes(Group, log2CPM)) +
+  geom_boxplot(aes(fill=Group), alpha=0.5, outliers=FALSE, col="black") +
+  geom_jitter(aes(fill=Group), pch=21, width=0.2, height=0, size=0.5) +
   scale_fill_manual(values=cols.group) +
-  facet_wrap(~Treatment, scales="free_y", ncol=1) +
-  scale_x_continuous(breaks=samp.days) +
-  guides(fill=guide_legend(ncol=2, override.aes=list(pch=21)),
-         shape="none") +
-  labs(x="Days postinfection",
-       y=element_blank(),
-       fill=element_blank()) +
-  theme(legend.position=c(0.8, 0.77))
-supl.fig$A
+  facet_wrap(~Cell.Type, ncol=5, scales="free_y") +
+  labs(x="Disease state",
+       y="Mean log2CPM") +
+  theme(legend.position="none",
+        axis.text.x=element_text(angle=45, hjust=1))
+ggsave("analysis/celltypes-controls.png", 
+       units="in", width=7.5, height=5)
+
+# plot cell types: treated only
+anno %>%
+  filter(Treatment=="4' FIU") %>%
+  ggplot(aes(Group, log2CPM)) +
+  geom_boxplot(aes(fill=Group), alpha=0.5, outliers=FALSE, col="black") +
+  geom_jitter(aes(fill=Group), pch=21, width=0.2, height=0, size=0.5) +
+  scale_fill_manual(values=cols.group) +
+  facet_wrap(~Cell.Type, ncol=5, scales="free_y") +
+  labs(x="Disease state",
+       y="Mean log2CPM") +
+  theme(legend.position="none",
+        axis.text.x=element_text(angle=45, hjust=1))
+ggsave("analysis/celltypes-treated.png", 
+       units="in", width=7.5, height=5)
+
+# plot cell types: treated vs. controls
+pvalue <- anno %>%
+          filter(Group!="Recovered",
+                 Group!="Early") %>%
+          mutate(Group=factor(Group, 
+                              levels=c("Baseline", "Middle", 
+                                       "Transitional", "Late"),
+                              labels=c("Baseline", "Middle", 
+                                       "Transitional/Late",
+                                       "Transitional/Late"))) %>%
+          group_by(Cell.Type, Group) %>%
+          summarise(pvalue=wilcox.test(log2CPM ~ Treatment)$p.value,
+                    y.position=max(log2CPM)+0.2,
+                    .groups="drop") %>%
+          filter(pvalue <= 0.05) %>%
+          mutate(xmin=as.integer(Group)-0.25,
+                 xmax=as.integer(Group)+0.25) %>%
+          rstatix::add_significance(p.col="pvalue", output.col="pvalue")
+supl.fig$E <- anno %>%
+              filter(Group!="Recovered",
+                     Group!="Early") %>%
+              mutate(Group=factor(Group, 
+                                  levels=c("Baseline", "Middle", 
+                                           "Transitional", "Late"),
+                                  labels=c("Baseline", "Middle", 
+                                           "Transitional/Late",
+                                           "Transitional/Late"))) %>%
+              ggplot(aes(Group, log2CPM)) +
+              geom_boxplot(aes(fill=Treatment), alpha=0.5, 
+                           outliers=FALSE, col="black") +
+              geom_point(aes(fill=Treatment, group=Treatment), 
+                          pch=21, size=0.5, 
+                         position=position_jitterdodge(jitter.height=0, 
+                                                       jitter.width=0.2)) +
+              scale_fill_manual(values=cols.treat) +
+              ggpubr::geom_bracket(data=pvalue, aes(label=pvalue)) +
+              facet_wrap(~Cell.Type, ncol=5, scales="free_y") +
+              # modulator to increase the y-axis limit of plots 
+              # with significance bars
+              geom_blank(data=pvalue, aes(y=y.position+0.6)) +
+              labs(x="Disease state",
+                   y="Mean log2CPM") +
+              theme(legend.position=c(0.9, -0.1),
+                    axis.text.x=element_text(angle=45, hjust=1))
+supl.fig$E
+ggsave("analysis/celltypes-comparison.png", 
+       units="in", width=7.5, height=5)
+
+# subset figure
+typelist <- c("B-cells", "NK cells", "Neutrophils", "Macrophages")
+pvalue <- filter(pvalue, Cell.Type %in% typelist)
+main.fig$E <- anno %>%
+              filter(Group!="Recovered",
+                     Group!="Early",
+                     Cell.Type %in% typelist) %>%
+              mutate(Group=factor(Group, 
+                                  levels=c("Baseline", "Middle", 
+                                           "Transitional", "Late"),
+                                  labels=c("Baseline", "Middle", 
+                                           "Transitional/Late",
+                                           "Transitional/Late"))) %>%
+              ggplot(aes(Group, log2CPM)) +
+              geom_boxplot(aes(fill=Treatment), alpha=0.5, 
+                           outliers=FALSE, col="black") +
+              geom_point(aes(fill=Treatment, group=Treatment), 
+                         pch=21, size=0.5, 
+                         position=position_jitterdodge(jitter.height=0, 
+                                                       jitter.width=0.2)) +
+              scale_fill_manual(values=cols.treat) +
+              ggpubr::geom_bracket(data=pvalue, aes(label=pvalue)) +
+              facet_wrap(~Cell.Type, nrow=1, scales="free_y") +
+              # modulator to increase the y-axis limit of plots 
+              # with significance bars
+              geom_blank(data=pvalue, aes(y=y.position+0.6)) +
+              labs(x="Disease state",
+                   y="Mean log2CPM") +
+              theme(legend.position="right",
+                    axis.text.x=element_text(angle=45, hjust=1))
+main.fig$E
+ggsave("analysis/celltypes-highlight.png", 
+       units="in", width=7.5, height=3)
+
+## pathways --------------------------------------------------------------------
+# what are the main differences between transitional vs late?
+pathways <- read.csv("analysis/ipa-output-controls.csv") %>%
+            select(Type, Name, Middle, Late) %>%
+            mutate(Treatment="Control")
+pathways <- read.csv("analysis/ipa-output-treated.csv") %>%
+            select(Type, Name, Middle, Transitional) %>%
+            rename(Late=Transitional) %>%
+            mutate(Treatment="FIU") %>%
+            rbind(pathways) %>% 
+            reshape2::melt(id.vars=c("Type", "Name", "Treatment"),
+                           variable.name="Group",
+                           value.name="zscore") %>%
+            reshape2::dcast(Type + Name + Group ~ Treatment, 
+                            value.var="zscore", fill=0) %>%
+            # filter to late/transitional time points only
+            filter(Group=="Late")
+
+# what has the largest differences?
+pathways %>%
+  mutate(Difference=FIU-Control) %>%
+  filter(Difference != 0) 
+
+# subset to shortlist with nicknames
+main.fig$F <- read.csv("analysis/ipa-output-shortlist.csv") %>%
+              left_join(pathways, by="Name") %>%
+              # make a "difference" column for sorting
+              mutate(Difference=FIU-Control) %>%
+              arrange(FIU, abs(Difference)) %>%
+              mutate(Nickname=factor(Nickname, levels=Nickname)) %>%
+              reshape2::melt(id.vars=c("Nickname", "Type"),
+                             measure.vars=c("Control", "FIU"),
+                             variable.name="Treatment",
+                             value.name="zscore") %>%
+              mutate(Treatment=factor(Treatment, levels=c("Control", "FIU"),
+                                      labels=c("Control, late", 
+                                               "4' FIU, transitional")),
+                     Type=factor(Type, 
+                                 levels=c("Canonical", "Functional", 
+                                          "Upstream"),
+                                 labels=c("Pathways", "Pathways", 
+                                          "Regulators"))) %>%
+              arrange(Type) %>%
+              mutate(Nickname=factor(Nickname, levels=unique(Nickname))) %>%
+              ggplot(aes(zscore, Nickname)) +
+              geom_vline(xintercept=0, linetype=2, col="grey") +
+              geom_line(aes(group=Nickname, linetype=Type)) +
+              geom_point(aes(fill=Treatment, shape=Treatment), size=2) +
+              scale_fill_manual(values=as.character(cols.treat)) +
+              scale_shape_manual(values=as.numeric(shapes.treat)) +
+              labs(x="z-score",
+                   y=element_blank(),
+                   fill=element_blank(),
+                   shape=element_blank(),
+                   linetype=element_blank()) +
+              theme(legend.position="right")
+main.fig$F
+ggsave("analysis/ipa-late.png",
+       units="in", width=7.5, height=3)
+
+## assemble figures ------------------------------------------------------------
+# main figure
+x <- cowplot::plot_grid(plotlist=main.fig[c("A", "B", "C", "D")], labels="AUTO")
+cowplot::plot_grid(x, main.fig$E, main.fig$F, labels=c(NA, "E", "F"), 
+                   ncol=1, rel_heights=c(2, 1, 1))
+ggsave("analysis/figure-main.png", units="in", width=7.5, height=12)
 
 # supplemental figure requires more paneling
 x <- cowplot::plot_grid(supl.fig$A, supl.fig$B, nrow=1, rel_widths=c(2, 1), 
                         labels="AUTO")
-cowplot::plot_grid(x, supl.fig$C, supl.fig$D, ncol=1, labels=c(NA, "C", "D"))
-ggsave("analysis/figure-supplemental.png", units="in", width=7.5, height=9)
+cowplot::plot_grid(x, supl.fig$C, supl.fig$D, supl.fig$E, 
+                   ncol=1, labels=c(NA, "C", "D", "E"),
+                   rel_heights=c(3, 3, 3, 4))
+ggsave("analysis/figure-supplemental.png", units="in", width=7.5, height=13)
+
+# clean up
+rm(x)
 
 ## fin -------------------------------------------------------------------------
 sessionInfo()
